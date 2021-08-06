a96fd60b0a5075d01eb2fd9ff8b38c090d5ad243
Rafael Malcervelli <48256253+MalcerOne@users.noreply.github.com>
2021-08-05 21:49:01 -0300

    first commit

---
+++ webScraper.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+"""
+This program acts like a web scraper to obtain data from a webs application.
+"""
+
+# Import necessary modules
+import time
+
+# Infos about the program
+__author__ = "Rafael Seicali Malcervelli"
+
+# Globals
+counter = 0
+
+# Function to translate the url to file
+def example():
+    return 0
