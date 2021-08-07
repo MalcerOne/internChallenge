258ffa1489d1e67a073c11a552a66bb87bd0ddb1
Rafael Malcervelli <48256253+MalcerOne@users.noreply.github.com>
2021-08-07 20:18:09 -0300

    avancando para o item 6, melhorar beautifulsoup

--- webScraper.py
+++ webScraper.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 """
-This program acts like a web scraper to obtain data from a webs application.
+This program is a web scraping robot to obtain data from a web application and save it to an excel datasheet.
 """
 
 # Import necessary modules
-# For problems with beautiful soup, discomment the line above
-#!pip install beautifulsoup4
 import requests
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 import time
+import pandas as pd
 
 # Infos about the program
 __author__ = "Rafael Seicali Malcervelli"
@@ -18,6 +17,10 @@ __author__ = "Rafael Seicali Malcervelli"
 # Globals
 counter = 0
 url = "https://esaj.tjsp.jus.br/cjpg/"
+
+# Web driver from google chrome. In case of any problems, download the driver
+# from this website: https://sites.google.com/a/chromium.org/chromedriver/downloads
+# and then put the file in the folder below.
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 
 # Requests
@@ -32,8 +35,10 @@ dataInicio = chrome.find_element_by_id("iddadosConsulta.dtInicio")
 dataFim = chrome.find_element_by_id("iddadosConsulta.dtFim")
 pesquisar = chrome.find_element_by_id("pbSubmit")
 
-
+# Robot actions
+# -----------------Pesquisa------------------
 pesquisa.send_keys("covid") # 2)
+
 classe.send_keys("Despejo") # 3)
 livre.click()
 time.sleep(2)
@@ -43,13 +48,25 @@ selecionar = chrome.find_element_by_xpath('//*[@id="classe_treeSelectContainer"]
 selecionar.click()
 
 
-
 dataInicio.send_keys("01/01/2020") # 4)
 dataFim.send_keys("31/12/2020") # 4)
 pesquisar.click()
 
+time.sleep(2)
+# -----------------Pesquisa------------------
+
+# -----------Getting data from HTML----------
+soupURL = chrome.current_url
+page = requests.get(soupURL)
+soup = BeautifulSoup(page.content, "html.parser")
+
+tableProcessos = soup.find("td", {"id": "tdResultados"})
+itemsTable = tableProcessos.table.find_all("tr")
+print(len(itemsTable))
+# -----------Getting data from HTML----------
 
 
-#soup = BeautifulSoup(page.content, 'html.parser')
+# Para passar para um excel, usar PANDAS
+
 
 
